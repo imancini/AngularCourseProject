13edb60aa15ea59842ab9122c298a3d87860040f
Isaac Mancini <imancini@imancini-mbp.local>
2018-04-12 16:49:08 -0700

    basic html laid out

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is simply a test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
+    new Recipe('Test Recipe', 'This is simply a test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
     new Recipe('Test Recipe', 'This is simply a test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
   ];
 
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class Ingredient {
+    // using typescript shorthand to create this class and construct it oin one line
+    constructor(public name: string, public amount: number){}
+}
